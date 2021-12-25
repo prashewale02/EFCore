221885f1aa463b51975d1b800fcdee7d818d500b
Static-PC <prashewale@gmail.com>
2021-12-26 02:17:46 +0530

    Code First approach to the database

---
+++ EFCore.DBLibrary/InventoryDbContext.cs
@@ -0,0 +1,58 @@
+﻿using EFCore.InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore.DBLibrary
+{
+    public class InventoryDbContext : DbContext
+    {
+        #region Private Members
+
+        private static IConfigurationRoot? _configuration;
+
+        #endregion
+
+
+        #region public Properties
+
+        public DbSet<Item> Items { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor for scaffolding
+        /// </summary>
+        public InventoryDbContext() { }
+
+        /// <summary>
+        /// The Complex constructor for allowing dependency injection.
+        /// </summary>
+        /// <param name="options"></param>
+        public InventoryDbContext(DbContextOptions options)
+            : base(options) 
+        {
+            // Intentionally Empty
+        }
+
+        #endregion
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json",
+                                    optional: true, reloadOnChange: true);
+
+                _configuration = builder.Build();
+                var connectionString =
+                    _configuration.GetConnectionString("InventoryManager");
+
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
--- EFCore.Sample/Program.cs
+++ EFCore.Sample/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCore.DBLibrary;
+using EFCore.InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore.Sample
+{
+    class Program
+    {
+        private static IConfigurationRoot? _configuration;
+        private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+
+        static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+        }
+
+        private static void EnsureItems()
+        {
+            EnsureItem("Batman Begins");
+            EnsureItem("Inception");
+            EnsureItem("Remember the Titans");
+            EnsureItem("Star Wars: The Empire Strikes Back");
+            EnsureItem("Top Gun");
+        }
+
+        private static void EnsureItem(string name)
+        {
+            using (var db = new InventoryDbContext(_optionsBuilder.Options))
+            {
+                // Determine if item exist:
+                var existingItem = db.Items
+                    .FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
+
+                if (existingItem == null)
+                {
+                    // Doesn't exist, add it.
+                    var item = new Item() { Name = name };
+                    db.Items.Add(item);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        private static void ListInventory()
+        {
+            using (var db = new InventoryDbContext(_optionsBuilder.Options))
+            {
+                var items = db.Items.OrderBy(x => x.Name).ToList();
+                items.ForEach(x => Console.WriteLine($"New Item: {x.Name}"));
+            }
+        }
+
+
+        static void Main(String[] args)
+        {
+            BuildOptions();
+            EnsureItems();
+            ListInventory();
+        }
+
+    }
+}
